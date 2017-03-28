ce767fc4f4b2ff129068b34e491d37603b454e87
michael <tracetech4@gmail.com>
2017-03-28 16:54:27 +0100

    giving a desired extension type

--- www/test.php
+++ www/test.php
@@ -19,9 +19,13 @@ $conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
 	#MAX FILE SIZE
 	define("MAX_FILE_SIZE", "2097152");
 
+	#ALLOWED EXTENSION
+	$ext = ["image/jpg", "image/jpeg", "image/png"];
+
 	if(array_key_exists('save', $_POST)) {
 
 		$errors = [];
+		print_r($_FILES); exit();
 
 		# be sure if a file was selected
 		if(empty($_FILES['pic']['name'])) {
@@ -32,6 +36,12 @@ $conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
 		if($_FILES['pic']['size'] > MAX_FILE_SIZE) {
 			$errors[] = "file size exceeds maximum. maximum: ". MAX_FILE_SIZE;
 		}
+
+		#check extension
+		if(!in_array($_FILES['pic']['type'], $ext)){
+			$errors[] = "invalid file type";
+		}
+
 		#print_r($_FILES);
 		if(empty($errors)){
 			echo "done";
