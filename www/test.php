2221c0c745c1a29726235f6fe490d2442ae753b4
michael <tracetech4@gmail.com>
2017-03-28 11:16:25 +0100

    using exception handling to handle error, by block catching it

--- www/test.php
+++ www/test.php
@@ -4,7 +4,18 @@ define('DBNAME', 'store');
 define('DBUSER', 'root');
 define('DBPASS', 'root');
 
+try{ 
+		#prepare a PDO INSTNCE
+
 $conn = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+	# SET verbose error modes
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+} catch(PDOException $e) {
+
+		echo $e->getMessage();
+}
 
 
 
