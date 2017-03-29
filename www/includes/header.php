900a296f25fd4691ac224be238e9a96243cefad3
michael <tracetech4@gmail.com>
2017-03-29 10:08:12 +0100

    the header template

---
+++ www/includes/header.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+	<title>Register</title>
+	<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+<body>
+	<section>
+		<div class="mast">
+			<h1>T<span>SSB</span></h1>
+		</div>
+	</section>
