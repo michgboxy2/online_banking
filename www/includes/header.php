740d229454379a4bb49ea03ab847066d69dde324
michael <tracetech4@gmail.com>
2017-03-29 10:46:42 +0100

    editting the header with /..

--- www/includes/header.php
+++ www/includes/header.php
@@ -2,7 +2,7 @@
 <head>
 	<title>Register</title>
 	<!DOCTYPE html>
-<link rel="stylesheet" type="text/css" href="styles/styles.css">
+<link rel="stylesheet" type="text/css" href="../styles/styles.css">
 </head>
 <body>
 	<section>
