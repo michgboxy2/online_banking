15df9c1c4947d6f675658893a2b40c66f5f620f2
michael <tracetech4@gmail.com>
2017-04-02 09:34:12 +0100

    form for edit

---
+++ www/edit_category.php
@@ -0,0 +1,37 @@
+<?php
+
+include 'includes/db.php';
+
+include 'includes/cat_header.php';
+
+include 'includes/function.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>EDIT CATEGORY</title>
+</head>
+<body>
+
+<form id="edit" action="view_category.php" method="post">
+
+
+</section>
+	<div class="wrapper">
+		<div id="stream">
+
+
+
+<label>EDIT</label>
+<input type="text" name="edit" placeholder="edit">
+	
+
+
+
+</form>
+
+</body>
+</html>
