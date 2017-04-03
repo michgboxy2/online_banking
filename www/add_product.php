961ed78c7a8c6ed2fbbabe02d108cc2dc01b415b
michael <tracetech4@gmail.com>
2017-04-03 07:47:43 +0100

    form field of the add product page

---
+++ www/add_product.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<form id="add" action="add_product.php" method="post">
+
+<label>BOOK TITLE:</label>
+<input type="text" name="btitle" placeholder="book name">
+
+<label>AUTHOR:</label>
+<input type="text" name="bauthor" placeholder="Author">
+
+<input type="hidden" name="cat_id">
+
+<label>PRICE:</label>
+<input type="text" name="bprice" placeholder="Price of book">
+
+<label>Year Of Publication</label>
+<input type="text" name="year" placeholder="year">
+
+<label>ISBN</label>
+<input type="text" name="isbn" placeholder="isbn">
+
+	
+
+
+
+
+</form>
+
+
+</body>
+</html>
