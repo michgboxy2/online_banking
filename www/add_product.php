37340753e4b8e70591efe42077869d49ee3db916
michael <tracetech4@gmail.com>
2017-04-03 07:59:31 +0100

    validating the add product form

--- www/add_product.php
+++ www/add_product.php
@@ -1,3 +1,42 @@
+<?php
+
+include 'includes/db.php';
+
+include 'includes/bookhead.php';
+
+include 'includes/function.php';
+
+
+?>
+
+<?php
+
+if(array_key_exists('submit', $_POST)){
+
+	$error = [];
+
+if(empty($_POST['btitle'])){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,24 +44,56 @@
 </head>
 <body>
 
+		</section>
+	<div class="wrapper">
+		<div id="stream">
+			<table id="tab">
+				<thead>
+					<tr>
+						<th>BOOK Title</th>
+						<th>Author</th>
+						 <th>Price</th> 
+						<th>Year of publication</th> 
+						<th>ISBN</th>
+					</tr>
+				<!-- </thead> -->
+				<!-- <tbody> -->
+					<!-- <tr> -->
+						<!-- <td>the knowledge gap</td> -->
+						<!-- <td>maja</td> -->
+						<!-- <td>January, 10</td> -->
+						<!-- <td><a href="#">edit</a></td> -->
+						<!-- <td><a href="#">delete</a></td> -->
+					</tr>
+          		</tbody>
+			</table>
+		</div>
+
 <form id="add" action="add_product.php" method="post">
 
 <label>BOOK TITLE:</label>
-<input type="text" name="btitle" placeholder="book name">
+<input type="text" name="btitle" placeholder="book name"/></br>
+</br>
 
 <label>AUTHOR:</label>
-<input type="text" name="bauthor" placeholder="Author">
+<input type="text" name="bauthor" placeholder="Author"/></br>
+</br>
 
-<input type="hidden" name="cat_id">
+<input type="hidden" name="cat_id"></br>
 
 <label>PRICE:</label>
-<input type="text" name="bprice" placeholder="Price of book">
+<input type="text" name="bprice" placeholder="Price of book"></br>
+</br>
 
 <label>Year Of Publication</label>
-<input type="text" name="year" placeholder="year">
+<input type="text" name="year" placeholder="year"></br>
+</br>
 
 <label>ISBN</label>
-<input type="text" name="isbn" placeholder="isbn">
+<input type="text" name="isbn" placeholder="isbn"></br>
+</br>
+
+<input type="submit" name="submt" value="Add book">
 
 	
 
