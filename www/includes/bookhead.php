e959a628ab44d2af076497fd2830b4ece9997f34
michael <tracetech4@gmail.com>
2017-04-05 14:01:12 +0100

    edit_product

---
+++ www/includes/bookhead.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TEST</title>
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+
+<body>
+	<section>
+		<div class="mast">
+			<h1>T<span>SSB</span></h1>
+			<nav>
+				<ul class="clearfix">
+					<li><a href="add_product.php" class="selected">DASHBOARD</a></li>
+					<li><a href="author">view posts</a></li>
+					<li><a href="#">logout</a></li>
+				</ul>
+			</nav>
+		</div>
+
+		
