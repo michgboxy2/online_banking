279c31bfa9740100e72e89a4c377fca8154eb781
michael <tracetech4@gmail.com>
2017-04-08 04:04:16 +0100

    add users page done

--- www/add_user.php
+++ www/add_user.php
@@ -17,133 +17,130 @@
 	#include 'styles/styles.css';
 
 		$errors = [];
-	if(array_key_exists('register', $_POST)){
-		# cache errors
-		
+	if(array_key_exists('submit', $_POST)){
+	
 
-		#validate firstname
-		if(empty($_POST['fname'])){
-			$errors['fname'] = "please enter your first name</br>" ;
+	if(empty($_POST['fname'])){
 
+			$errors['fname'] = "please enter your firstname";
 		}
 
-		
+	
+	if(empty($_POST['lname'])){
 
-		if(empty($_POST['lname'])){
-			$errors['lname'] = "please enter your last name</br>";
-		}
+			$errors['lname'] = "please enter last name";
+	}
 
-		if(empty($_POST['email'])){
-			$errors['email'] = "please enter your email</br>";
-		}
 
-		if(doesEmailExist($conn, $_POST['email'])){
+	if(empty($_POST['email'])){
+
+			$errors['email'] = "please enter email";
+	}
+
+	if(doesUserEmailExist($conn, $_POST['email'])){
+
 			$errors['email'] = "email already exists";
 		}
 
-		if(empty($_POST['password'])){
-			$errors['password'] = "please enter your password</br>";
-		}
+	if(empty($_POST['username'])){
 
-		if($_POST['password'] != $_POST['pword']){
-			$errors['pword'] = "passwords do not match</br>";
-		} 
+			$errors['username'] = "please enter your username";
+	}
 
+	if(empty($_POST['password'])){
 
-		if(empty($errors)){
-			//do database stuff
+			$errors['password'] = "please enter password";
+	}
 
-			#eliminate unwanted spaces from values in the $_post array
+	if($_POST['password'] != $_POST['pword']){
 
-			$clean = array_map('trim', $_POST);
+			$errors['pword'] = "password doesnt match, please try again";
+	}
 
-			#register admin
+	if(empty($errors)){
 
-			doAdminRegister($conn, $clean);
+		$clean = array_map('trim', $_POST);
 
-			#hash the password
-
-			/*$hash = password_hash($clean['password'], PASSWORD_BCRYPT);
-
-			#insert data
-
-			$stmt = $conn->prepare("INSERT INTO admin(firstname, lastname, email, hash) VALUES(:fn, :ln, :e, :h)");
-
-			#bind params...
-			$data = [
-				':fn' => $clean['fname'],
-				':ln' => $clean['lname'],
-				':e' => $clean['email'],
-				':h' => $hash
-
-				];
-
-				$stmt->execute($data); */
-		
-			
-			
-			
-		}
-
+		userRegister($conn, $clean);
 
 	}
+
+	foreach($errors as $errors){
+
+		echo "<p>".$errors."</p>";
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
 ?>
 
 
 <div class="wrapper">
-		<h1 id="register-label">Admin Register</h1>
-		<hr>
-		<form id="register"  action ="register.php" method ="POST">
-			<div> <?php
+		<form id="register" action="add_user.php" method="post">
+
+		<?php displayErrors($errors,'fname');    ?>
+		<label>FIRST NAME</label>
+		<input type="text" name="fname" placeholder="first name"></br>
+		</br>
+		</br>
 
 
-					#if(isset($errors['fname'])) { echo '<span class="err">'.$errors['fname']. '</span>';}
-					$display = displayErrors($errors,'fname');
+		<div>
+		<?php displayErrors($errors,'lname');    ?>
+		<label>LAST NAME</label>
+		<input type="text" name="lname" placeholder="last name"></br>
+		</div>
+		</br>
+		</br>
+		
+		<?php displayErrors($errors,'email');    ?>
+		<label>Email</label>
+		<input type="text" name="email" placeholder="email address"></br>
+		</br>
+		</br>
 
-					echo $display;
+		<?php if(isset($errors['username'])){ echo '<span class="$err">'.$errors['username']. '</span>'; } ?>
+		<label>Username</label>
+		<input type="text" name="username" placeholder="username"></br>
+		</br>
+		</br>
 
-					?>
+		<label>Password</label>
+		<input type="password" name="password" placeholder="password"></br>
+		</br>
+		</br>
 
-				<label>first name:</label>
-				<input type="text" name="fname" placeholder="first name">
-			</div>
+		<label>confirm password</label>
+		<input type="password" name="pword" placeholder="confirm password"></br>
+		</br>
+		</br>
+
+		<input type="submit" name="submit" value="signup"></br>
 			
-			<div><?php 
-					if(isset($errors['lname'])) {echo '<span class="err">'.$errors['lname']. '</span';}
 
-				  ?>
-				<label>last name:</label>	
-				<input type="text" name="lname" placeholder="last name">
-			</div>
 
-			<div><?php if(isset($errors['email'])) {echo '<span class="err">'.$errors['email']. '</span>';}
 
-					?>
-				<label>email:</label>
-				<input type="text" name="email" placeholder="email">
-			</div>
-			
-			<div><?php if(isset($errors['password'])) {echo '<span class="err">'.$errors['password']. '</span>';}
 
-			?>
-				<label>password:</label>
-				<input type="password" name="password" placeholder="password">
-			</div>
- 
-			<div><?php if(isset($errors['pword'])){ echo '<span class="err">'.$errors['pword']. '</span>';}
-			?>
-				<label>confirm password:</label>	
-				<input type="password" name="pword" placeholder="password">
-			</div>
 
-			<input type="submit" name="register" value="register">
+
+
+
+
+
+
+
+
+
+
+
+
 		</form>
-
-		<h4 class="jumpto">Have an account? <a href="login.php">login</a></h4>
-	</div>
-
-	<?php
-
-	include 'includes/footer.php';
-
-	?>
